1cd9774ff30d58bd612f372136ba65d5d2d9cff6
Louis Godlewski <louis.godlewski@gmail.com>
2020-09-10 20:52:06 +0200

    Fixing & adding condition
    - Fixing dml (duplicate member)
    - Adding info on Payment modal
    - Improving requests after payment
    - Updating filteredMembers (only > 1 members shown)

--- application/web-ui/src/app/components/views/jar-details/jar-over/pay/pay.component.ts
+++ application/web-ui/src/app/components/views/jar-details/jar-over/pay/pay.component.ts
@@ -1,14 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef, Inject } from '@angular/core';
-import { JarDetails } from 'src/app/models';
-import { FormControl, Validators } from '@angular/forms';
-import { Patterns } from 'src/app/services/forms/utils';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ConfessComponent } from 'src/app/components/shared/confess/confess.component';
-import { JarDialogData } from 'src/app/models/jar/jar-data.dialog';
-import { ConfessionService } from 'src/app/services/domain/jar/member/confession.service';
-import { JarService } from 'src/app/services/domain/jar/jar.service';
-import { Router } from '@angular/router';
+import { PaymentDialogData } from 'src/app/models';
 
 @Component({
     selector: 'app-pay',
@@ -17,26 +9,11 @@ import { Router } from '@angular/router';
 })
 export class PayComponent {
 
-    @ViewChild('confessionInput', { static: false }) confessionInput: ElementRef<HTMLInputElement>;
-
-    get jar(): JarDetails { return this.data.jar; }
-    get reporterId(): number { return this.jar.members.find(member => member.userId === this.data.user).id; }
-
     isChecked = false;
 
-    readonly confession = new FormControl('', [Validators.required, Validators.pattern(Patterns.alphanumeric)]);
+    constructor(private dialogRef: MatDialogRef<PayComponent>, @Inject(MAT_DIALOG_DATA) public data: PaymentDialogData) {}
 
-    constructor(
-        protected snackBar: MatSnackBar, protected dialogRef: MatDialogRef<ConfessComponent>, private jarService: JarService,
-        @Inject(MAT_DIALOG_DATA) public data: JarDialogData, private router: Router) {}
-
-    pay(): void {
-        this.jarService.pay(this.jar.id).subscribe( () => {
-            this.close();
-            this.router.navigate(['/dashboard']);
-            this.snackBar.open('You supported the association with success !', 'close', { duration: 3000 });
-        }, err => this.snackBar.open(err, 'close', { duration: 3000 } ));
-    }
+    pay = (): void => this.dialogRef.close(this.isChecked);
 
     close = (): void => this.dialogRef.close();
 
---
+++ application/web-ui/src/app/models/utils/dialog/payment-data.dialog.ts
@@ -0,0 +1,5 @@
+export interface PaymentDialogData {
+    balance: number;
+    jarId: number;
+    addresseeLink: string;
+}
